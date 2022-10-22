a23eb84449cbd1dbfb92765ca0e544c96434a0e8
Om Official <55656898+OmBharti999@users.noreply.github.com>
2022-10-22 18:58:30 +0530

    Initial commit

---
+++ src/pages/advice/Advice.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import "./advice.css";
+
+export default function Advice() {
+  const [advice, setAdvice] = useState("");
+
+  useEffect(() => {
+    fetchQ();
+  }, []);
+
+  async function fetchQ() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+  }
+  return (
+    <div className="advice-container">
+      <div className="advice-text">
+        <div>{advice}</div>
+        <button onClick={fetchQ} className="btn-advice">
+          Click
+        </button>
+      </div>
+    </div>
+  );
+}
